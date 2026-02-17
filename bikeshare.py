55b4980d82743ba1e7a278bf7f0dcc8581b3984c
Shweta goel <shweta.a.goel@accenture.com>
2026-02-17 06:27:17 +0000

    Adding code to bikeshare.py

---
+++ bikeshare.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+def load_data(city):
+    #Load data
+    pass
+
+def calculate_stats(data):
+    #Calculate
+    pass
+
+if __name__=="_main":
+    city = input("Enter the city name:")
+    data=load_data(city)
+    stats=calculate_stats(data)
+    print(stats)
